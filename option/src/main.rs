9cc07fc7b42be54b3aba65c3574a790d416cac8e
rohith anshumaan <thescript356@gmail.com>
2024-12-01 14:00:17 +0530

    minor updates

--- option/src/main.rs
+++ option/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let option_return = find_first_a(String::from("Rohith"));
+    let option_return = find_first_a(String::from("Abhinav"));
     match option_return {
         Some(val) => println!("'a' found at index {}", val),
         None => println!("'a' not found in the string.")
